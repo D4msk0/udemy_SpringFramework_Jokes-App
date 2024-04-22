a16d6da43820f865133167b57141bbf307fefa5d
Merry Damsko <m.d4msk0@gmail.com>
2024-04-22 14:55:07 +0200

    first commit

---
+++ src/main/java/damsko/springframework/udemy_SpringFramework_JokesApp/UdemySpringFrameworkJokesAppApplication.java
@@ -0,0 +1,13 @@
+package damsko.springframework.udemy_SpringFramework_JokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringFrameworkJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringFrameworkJokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damsko/springframework/udemy_SpringFramework_JokesApp/UdemySpringFrameworkJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package damsko.springframework.udemy_SpringFramework_JokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringFrameworkJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

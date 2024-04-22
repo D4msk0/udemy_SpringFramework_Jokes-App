41eb51bb5c32667ad6a2fe2d050fdb29efdf0b3f
Merry Damsko <m.d4msk0@gmail.com>
2024-04-22 15:35:18 +0200

    add JokeController

---
+++ src/main/java/damsko/springframework/udemy_SpringFramework_JokesApp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package damsko.springframework.udemy_SpringFramework_JokesApp.controllers;
+
+import damsko.springframework.udemy_SpringFramework_JokesApp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
